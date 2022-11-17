4fb6ca0929dc0b22b8a018368ef9ade173fbb125
daiki2828 <114804118+daiki2828@users.noreply.github.com>
2022-11-17 10:34:26 +0000

    【完了】管理者/商品一覧・詳細・編集・更新・新規登録

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,49 @@
 class Admin::ItemsController < ApplicationController
+  
+  def index
+    @items = Item.all
+  end
+    
+  def new
+    @item = Item.new
+    @genres = Genre.all
+  end
+  
+  def create
+    @item =Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item)
+    else
+      @genres = Genre.all
+      @item =Item.new
+      render:new
+    end
+  end
+  
+  def show
+    @item = Item.find(params[:id])
+  end
+  
+  def edit
+    @item = Item.find(params[:id])
+    @genres = Genre.all
+  end
+  
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+     redirect_to admin_item_path
+    else
+     @genres = Genre.all
+     @item = Item.new
+     render :edit
+    end
+  end
+  
+  private
+
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :image, :introduction, :price, :is_active)
+  end
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,20 @@
 class Item < ApplicationRecord
+  belongs_to :genre
+  has_many :order_items, dependent: :destroy
+  has_many :cart_items, dependent: :destroy
+  
+  has_one_attached :image
+  
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
+
+  def get_image
+    (image.attached?) ? image : 'no_image.jpg'
+  end
+  
+  def price_add_tax
+      tax = 1 + 0.10
+      ( self.price * tax).floor
+  end
 end
