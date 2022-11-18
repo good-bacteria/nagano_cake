c88e317de0782a22c6252fd59af548aeae795925
daiki2828 <114804118+daiki2828@users.noreply.github.com>
2022-11-18 07:51:21 +0000

    【途中3】顧客/商品一覧・詳細

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,9 +1,12 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.all 
-    @genres = Grenre.all
-  end 
-  
+    @items = Item.all
+    @genres = Genre.all
+  end
+
   def show
+    @item = Item.find(params[:id])
+    @genres = Genre.all
+    @cart_item = CartItem.new
   end
 end
