28eecc3d3ad3849a54fc274b3c9a7a51c342705d
urotansh <114655455+urotansh@users.noreply.github.com>
2022-11-18 09:08:56 +0000

    [Add] 顧客側注文機能のアクションとViewファイル(new/confirm/create/thanks/index/show)追加

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,22 @@
 class Public::OrdersController < ApplicationController
+  
+  def new
+  end
+  
+  def confirm
+  end
+  
+  def thanks
+  end
+  
+  def create
+    redirect_to orders_thanks_path
+  end
+  
+  def index
+  end
+  
+  def show
+  end
+  
 end
