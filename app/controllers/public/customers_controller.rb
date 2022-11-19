54237944ceb69d41f240d025c95d5cac21d1c966
EC2 Default User <amrumineq@gmail.com>
2022-11-19 04:22:12 +0000

    add public customer withdraw以外

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -16,6 +16,21 @@ class Public::CustomersController < ApplicationController
     end   
   end
   
+  def quit
+  end
+  
+  # テスト用にif 文で退会or有効に変更できる
+  # 退会後の挙動を追加予定
+  def withdraw
+    @customer = current_customer
+    if @customer.is_deleted == false
+      @customer.update(is_deleted: true)
+    else
+      @customer.update(is_deleted: false)
+    end
+    redirect_to customers_my_page_path
+  end
+  
   private
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :email, :is_deleted)
