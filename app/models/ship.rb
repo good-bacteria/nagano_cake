a42a383164910fa11b50410bc377cb54ebd2723d
EC2 Default User <amrumineq@gmail.com>
2022-11-21 08:12:05 +0000

    Add ship

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -2,6 +2,9 @@ class Ship < ApplicationRecord
   
   belongs_to :customer
   
+  validates :postal_code,presence:true ,format: { with: /\A\d{7}\z/ }
+  validates :address,presence:true
+  validates :name,presence:true
   
   def address_display
     '〒' + postal_code + ' ' + address + ' ' + name
