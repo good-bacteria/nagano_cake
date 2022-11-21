31673152646faf9d08e1826e73f39e2beb481449
EC2 Default User <amrumineq@gmail.com>
2022-11-21 06:31:02 +0000

    public customer fin

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -2,35 +2,31 @@ class Public::CustomersController < ApplicationController
   def show
     @customer = current_customer
   end
-  
+
   def edit
     @customer = current_customer
   end
-  
+
   def update
     @customer = current_customer
     if @customer.update(customer_params)
       redirect_to customers_my_page_path
     else
       render :edit
-    end   
+    end
   end
-  
+
   def quit
   end
-  
-  # テスト用にif 文で退会or有効に変更できる
-  # 退会後の挙動を追加予定
+
   def withdraw
     @customer = current_customer
-    if @customer.is_deleted == false
-      @customer.update(is_deleted: true)
-    else
-      @customer.update(is_deleted: false)
-    end
-    redirect_to customers_my_page_path
+    @customer.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行いたしました"
+    redirect_to root_path
   end
-  
+
   private
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :email, :is_deleted)
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -11,11 +11,11 @@ class Customer < ApplicationRecord
   validates :last_name,presence:true
   validates :first_name,presence:true
   validates :last_name_kana,presence:true ,format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい'}
-  validates :first_name_kana,presence:true , format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい'}
-  validates :postal_code,presence:true　,format: { with: /\A\d{7}\z/ }
+  validates :first_name_kana,presence:true ,format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい'}
+  validates :postal_code,presence:true ,format: { with: /\A\d{7}\z/ }
   validates :address,presence:true
   validates :phone_number,presence:true ,format: { with: /\A\d{10,11}\z/ }
-  validates :email,presence:true
+  validates :email,presence:true ,uniqueness: true
   
   def full_name
     self.last_name + self.first_name
@@ -24,5 +24,9 @@ class Customer < ApplicationRecord
   def full_name_kana
     self.last_name_kana + self.first_name_kana
   end
-
+  
+  def active_for_authentication?
+    super && (self.is_deleted == false)
+  end
+  
 end
