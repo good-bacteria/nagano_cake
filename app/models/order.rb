d0534414efaa035f8198b748ee07d4698e98960c
urotansh <114655455+urotansh@users.noreply.github.com>
2022-11-21 22:54:07 +0000

    [Add] Orderモデルへバリデーションを追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,16 @@ class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
   has_many :items, through: :order_items
   
+  validates :customer_id, presence: true
+  validates :postal_code, presence: true, format: { with: /\A\d{7}\z/, message: 'は半角数字7桁で入力して下さい'}
+  validates :address, presence: true
+  validates :name, presence: true
+  validates :shipping_cost, presence: true, numericality: true
+  validates :total_payment, presence: true, numericality: true
+  validates :payment_method, presence: true
+  validates :status, presence: true
+  
+  
   enum payment_method: { credit_card: 0, transfer: 1 }
   
   
