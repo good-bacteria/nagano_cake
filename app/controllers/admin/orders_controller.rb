8c8694853c1e627a85e8d425512efb3210532b1e
EC2 Default User <yotchan32323@gmail.com>
2022-11-22 07:28:31 +0000

    管理者側注文機能の実装

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,23 @@
 class Admin::OrdersController < ApplicationController
+  def show
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    if @order.status == "入金確認"
+      @order.order_items.update(making_status: 1)
+    end
+    redirect_to admin_order_path(@order)
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
+  end
+
+
 end
