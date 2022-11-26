b9eb7f270641e2e8df9d11df7e91800cd81bb011
urotansh <114655455+urotansh@users.noreply.github.com>
2022-11-26 01:28:26 +0000

    [Add] ジャンル検索機能の追加

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,15 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.page(params[:page]).per(8)
-    @genres = Genre.all
+    if params[:genre_search]
+      @genre = Genre.find(params[:genre_id])
+      @items = @genre.items.page(params[:page]).per(8)
+      @items_count = @genre.items.size
+      @genres = Genre.all
+    else
+      @items = Item.page(params[:page]).per(8)
+      @items_count = Item.all.size
+      @genres = Genre.all
+    end
   end
 
   def show
